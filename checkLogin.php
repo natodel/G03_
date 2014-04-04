71a8a3a4c5ade3c7f9f6230a8f6f6e3dbb7bd055
natodel <natodel@gmail.com>
2014-04-04 09:26:26 +0700

    Update login 4/4

--- checkLogin.php
+++ checkLogin.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<link rel="stylesheet" type="text/css" media="all" href="css/changeAccStyle.css" />
-  <link rel="shortcut icon" href="images/logo.png"  />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Thay đổi thông tin cá nhân</title>	
-</head>
-
-<body>
-	<header>
-  	<a href="index.php"><img class="logo" src="images/logo.png" alt="logo" title="Trang chủ Góc sẻ chia" width="82px" height="82px" /></a>
-   <hr size="5px" noshade color="#CCCCCC" />
- 	</header>
-  <div align="center" style="color:#FF0">
-  <br />
-	<?php
+<?php
 	if(!empty($_POST['userLogin']) && !empty($_POST['passLogin']))
 	{
 		require_once('config.php');
@@ -25,8 +9,8 @@
 		if(mysql_num_rows($query) == 0)
 		{
 			print <<<EOP
-				<h2>Sai thông tin</h2> <br>
-				<a href="index.php" style="font-size:19px;color:green;">Nhấn vào đây để đăng nhập lại</a>
+				Sai thông tin <br>
+				<a href="index.php">Nhấn vào đây để đăng nhập lại</a>
 EOP;
 		}
 		else
@@ -49,10 +33,7 @@ EOP;
 	}
 	else
 	{
-		echo '<h2>Nhập đủ tên đăng nhập và tài khoản :-p</h2>';
-		echo '<h2><a href="homepage.php">Nhấn vào đây để đăng nhập lại</a></h2>';
+		echo 'Nhập đủ tên đăng nhập và tài khoản :-p';
+		echo '<a href="homepage.php">Nhấn vào đây để đăng nhập lại</a>';
 	}
 ?>
-	</div>
-</body>
-</html>
