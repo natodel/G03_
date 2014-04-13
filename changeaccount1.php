07ef7978b83e8600d23397a21e702ac42955cfff
anhlt570 <anhletuan128@gmail.com>
2014-04-14 00:50:18 +0700

    13/4/2014

--- changeaccount1.php
+++ changeaccount1.php
@@ -1,46 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<link rel="stylesheet" type="text/css" media="all" href="css/changeAccStyle.css" />
-  <link rel="shortcut icon" href="images/logo.png"  />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Thay đổi thông tin cá nhân</title>	
-</head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<h1>Thông tin hiện tại của bạn</h1>
+<?php
+	session_start();
+	@mysql_connect("localhost", "root", "") or die ('Can not connect to host');
+	@mysql_select_db("data_userweb");
+	$user_curr = $_SESSION['user_curr'];
+	$refer = "select * from members where account='".$user_curr."'";
+	$query = mysql_query($refer);
+	$row_curr = mysql_fetch_array($query);
+	
+	echo "Tài khoản: ".$row_curr['account']."<br/>";
+	echo "Họ và tên: ".$row_curr['name']."<br/>";
+	echo "Địa chỉ email: ".$row_curr['email']."<br/>";
+	echo "Số điện thoại: ".$row_curr['phone']."<br/>";
+?>
 
-<body>
-	<header>
-  	<a href="index.php"><img class="logo" src="images/logo.png" alt="logo" title="Trang chủ Góc sẻ chia" width="82px" height="82px" /></a>
-   <hr size="5px" noshade color="#CCCCCC" />
- 	</header>
-   
-  <div align="center">	
-    <h1>Thông tin hiện tại của bạn</h1>
-    <font color="white">
-    <?php
-      session_start();
-      require_once('config.php');
-      $user_curr = $_SESSION['user_curr'];
-      $refer = "select * from members where account='".$user_curr."'";
-      $query = mysql_query($refer);
-      $row_curr = mysql_fetch_array($query);
-      
-      echo "<b>Tài khoản:</b> ".$row_curr['account']."<br/>";
-      echo "<b>Họ và tên:</b>	 ".$row_curr['name']."<br/>";
-      echo "<b>Địa chỉ email:</b> ".$row_curr['email']."<br/>";
-      echo "<b>Số điện thoại:</b> ".$row_curr['phone']."<br/>";
-    ?>
-  	</font>
-    <h1>Điền vào thông tin bạn muốn thay đổi</h1>
-  
-    <form action="changeaccount2.php" method="post">
-      <input type="text" name="name" class="styleBox" size="26" placeholder="Họ và tên" /><br />
-      <input type="text" name="email" class="styleBox" size="26" placeholder="Email"><br />
-      <input type="text" name="phone" class="styleBox" size="26" placeholder="Điện thoại"><br/>
-      <input type="password" name="password" class="styleBox" size="26" placeholder="Mật khẩu"><br />
-      <input type="password"  name="repassword" class="styleBox" size="26" placeholder="Nhập lại mật khẩu"><br/>
-     	&nbsp;<input type="password" name="pass_curr" class="styleBox" size="26" placeholder="Mật khẩu hiện tại"><b style="color:#F90">*</b><br />
-     	<input type="submit" name="edited" class="submitButton" style="border-radius:5px 5px 5px 5px;width:100px;height:30px;" value="Hoàn Thành">
-   	</form>
-  </div>
-</body>
-</html>
+<h1>Điền vào thông tin bạn muốn thay đổi</h1>
+
+<form action="changeaccount2.php" method="post">
+	<input type="text" name="name" placeholder="Họ và tên"> <br/>
+  <input type="password" name="password" placeholder="Mật khẩu"> <br/>
+  <input type="password"  name="repassword" placeholder="Nhập lại mật khẩu"><br/>
+  <input type="text" name="email" placeholder="Email"><br/>
+  <input type="text" name="phone" placeholder="Điện thoại"><br/>
+  <p style="font-size:18px;color:red;">Nhập mật khẩu hiện tại của bạn: </p> *<input type="password" name="pass_curr" placeholder="Mật khẩu hiện tại"><br/><br/>
+  <input type="submit" name="edited" value="Hoàn Thành">
+</form>
