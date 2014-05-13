fc113d46257dbe8ed670f6db660716acaeb8b37d
anhlt570 <anhletuan128@gmail.com>
2014-05-14 02:05:48 +0700

    update discussion.php

--- discussion.php
+++ discussion.php
@@ -20,6 +20,7 @@ session_start();
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <script src="javascripts/jquery-1.11.0.min.js" type="text/javascript"></script>
      
+     
     </head>
     <body>
         <?php
