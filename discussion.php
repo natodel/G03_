88a74069a3b40ca0974c074fac63ccbaf6858161
sonnh57 <sonnh57@github.com>
2014-05-14 01:28:01 +0700

    Update 14/05

--- discussion.php
+++ discussion.php
@@ -13,31 +13,67 @@ session_start();
         <link rel="stylesheet" type="text/css" media="all" href="css/containtSliders.css" />    
         <link href="css/discussionStyle.css" rel="stylesheet" type="text/css"/>
         <script src="javascripts/jquery-1.11.0.min.js" type="text/javascript"></script>
-     
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media 
+        queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page  
+        via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/
+        html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/
+        respond.min.js"></script>
+        <![endif]-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files 
+        as needed -->
+        <script src="javascripts/s/bootstrap.min.js"></script>
     </head>
-    <body>
-        <?php
-        include('header.php');
-        ?>
-        <div id="mainWrapper" >
-            <br/>
-            <div class= "index"  align="center">
-            	<div class="text">
-					<?php
-                    $topic = $_GET['topic'];
-                    require_once('config.php');
-                    $query = "SELECT body from topics where id='" . $topic . "';";
-                    $result = mysql_query($query);
-                    while ($topic = mysql_fetch_array($result)) {
-                        echo $topic['body'];
-                    }
-                    ?>
-                </div>
+<body>
+    <?php
+    include('header.php');
+    ?>
+    <div id="mainWrapper" >
+        <br/>
+        <div class= "index"  align="center">
+            <div class="text">
+
+                <?php
+                $topic = $_GET['topic'];
+                require_once('config.php');
+                $query = "SELECT * from topics where id='" . $topic . "';";
+                $result = mysql_query($query);
+                while ($topic = mysql_fetch_array($result)) {
+                    $_bodyDisplay = $topic['body'];
+                    $_username = $topic['username'];
+                }
+                ?>
+                <?php
+                $userQuery = "SELECT name from users where account='" . $_username . "';";
+                $userResult = mysql_query($userQuery);
+                while ($users = mysql_fetch_array($userResult)) {
+                    $_postUser = $users['name'];
+                }
+                ?>
+                <h2><?php echo $_postUser; ?></h2>
+                
+                <p>
+                    <?php echo $_bodyDisplay; ?>
+                </p>
+                
             </div>
-            <br/>
-            
-            <div class="comments">
-            	
+
         </div>
-    </body>
+        <br/>
+
+        <div>
+        </div>
+
+
+    </div>
+</body>
 </html>
---
+++ topicsManagment.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media 
+        queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page  
+        via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/
+        html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/
+        respond.min.js"></script>
+        <![endif]-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files 
+        as needed -->
+        <script src="javascripts/s/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <?php
+            require_once('config.php');
+            $query = "SELECT * from topics;";
+            $result = mysql_query($query);
+            if(isset($_GET['id'])){
+                $id = $_GET['id'];
+            }
+            else{
+                $id="";
+            }
+            ?>
+            <table class="table table-striped table-bordered table-hover table-condensed table-responsive">
+                <caption><h2>Quản lý thành viên</h2></caption>
+                <tr>
+                    <th>Tiêu đề</th>
+                    <th>Người đăng</th>
+                    <th>Ngày đăng</th>
+                    <th>Chuyên mục</th>
+                    <th>Công cụ</th>
+                </tr>
+                <?php
+                while ($topic = mysql_fetch_array($result)) {
+                    if($topic['id']===$id){
+                        echo "<form action='updateTopic.php' method='POST'><tr><td>" . $topic['subject'] . "</td>";
+                        echo "<td>" . $topic['username'] . "</td>";
+                        echo "<td><select class='form-control'><option value='giadinh'>Gia đình</option><option value='tinhyeu'>Tình yêu</option><option value='tinhban'>Tình bạn</option><option value='hoctapcongviec'>Học tập - Công việc</option><option value='thamkin'>Thầm kin</option></select><input type='hidden' name='userID' value='{$topic['id']}' /></td>";
+                        echo "<td>" . $topic['date'] . "</td>";
+                        echo "<td><a href='topicsManagment.php'>Hủy</a> || <button class='btn-link type='submit'>Hoàn thành</button></td></tr></form>";
+                    }
+                    else{
+                        echo "<tr><td><address><a href='discussion.php?topic='{$topic['id']}'>" . $topic['subject'] . "</a></address></td>";
+                        echo "<td>" . $topic['username'] . "</td>";
+                        echo "<td>" . $topic['date'] . "</td>";
+                        echo "<td>" . $topic['subject'] . "</td>";
+                        echo "<td><a href='?id={$topic['id']}'>Sửa</a> || <a href='deleteTopic.php?id={$topic['id']}'>Xóa</a></td></tr>";
+                    }
+                    
+                }
+                ?>
+            </table>
+           
+        </div>
+    </body>
+</html>
